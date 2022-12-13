6e137b1b04fb8a42a4f0b99ec042f0052f6d4aeb
huangchen1992 <hc_0117@qq.com>
2022-12-13 17:52:30 +0800

    delete *.o *.d

--- shellcode-inject/obj/local/arm64-v8a/objs/inject/proc_tool.o.d
+++
@@ -1,6 +0,0 @@
-./obj/local/arm64-v8a/objs/inject/proc_tool.o: jni/proc_tool.c \
-  jni/proc_tool.h jni/log_marco.h
-
-jni/proc_tool.h:
-
-jni/log_marco.h:
--- shellcode-inject/obj/local/armeabi-v7a/objs/inject/proc_tool.o.d
+++
@@ -1,6 +0,0 @@
-./obj/local/armeabi-v7a/objs/inject/proc_tool.o: jni/proc_tool.c \
-  jni/proc_tool.h jni/log_marco.h
-
-jni/proc_tool.h:
-
-jni/log_marco.h:
